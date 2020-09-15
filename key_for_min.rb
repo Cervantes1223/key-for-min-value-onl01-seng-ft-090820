77bf825b7f4c4ed80edfe0fab300b527efd7843c
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 16:11:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
-  name_hash = {"rob" => 2, "jake" => 12, "byan" => 45}
+def key_for_min_value
+ hash = {"rob" => 2, "jake" => 12, "byan" => 45}
   key_for_min_value(name_hash)
 end
