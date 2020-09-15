9e9d9f5e8e7877fd17b73f95bfeb9b497fee2bd6
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-15 16:08:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  name_hash = {"rob" => 2, "jake" => 12, "byan" => 45}
+  key_for_min_value(name_hash)
 
 end
